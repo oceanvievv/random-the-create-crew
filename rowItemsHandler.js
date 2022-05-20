0d0eeaf6f6f46b3363d2a24317273426e77d896c
Nikolay Petrov <oceanvievv@gmail.com>
2022-05-20 20:48:23 +0500

    non-adaptive page

---
+++ rowItemsHandler.js
@@ -0,0 +1,17 @@
+const rowWrapperInit = document.getElementById("rowWrapper");
+
+const row1 = document.getElementById("row1");
+const row2 = document.getElementById("row2");
+const row3 = document.getElementById("row3");
+
+if (window.screen.width > 425) {
+  const elToHandle1 = row2.children.item(0);
+  const elToHandle2 = row3.children.item(0);
+  const elToHandle3 = row3.children.item(1);
+
+  row1.appendChild(elToHandle1);
+  row2.appendChild(elToHandle2);
+  row2.appendChild(elToHandle3);
+
+  row3.remove();
+}
